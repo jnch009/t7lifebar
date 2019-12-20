73d8df2f269500ce26c0f00a6ad9dc8fa803a0a3
jnch009 <jngcheng@sfu.ca>
2019-12-19 21:31:42 -0800

    applying a shearing effect

--- t7lifebar.java
+++ t7lifebar.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp; // for applying the affine transformation
 
 public class t7lifebar extends JPanel{
 
@@ -15,6 +17,10 @@ public class t7lifebar extends JPanel{
 
      private Image createLifeBar(){
         BufferedImage bufferedImage = new BufferedImage(550,50,BufferedImage.TYPE_INT_RGB);
+        AffineTransform transform = new AffineTransform();
+        transform.shear(0.30, 0);
+        AffineTransformOp transformOp = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        bufferedImage = transformOp.filter(bufferedImage, null);
         Graphics g = bufferedImage.getGraphics();
 
         //yellow
@@ -29,21 +35,21 @@ public class t7lifebar extends JPanel{
         // END GOAL is to do bilinear interpolation, focus on 1 dimension first
         // formula is: c = a + (b-a)*t
 
-        g.setColor(start);
+        // g.setColor(start);
         
-        for (int j = 0; j <= 100; j++){
-            for (int i = 0; i <= 550; i++){
-                if (i > 0){
-                    float t = (float)i / 550;
-                    // TODO: refactor the following into a method
-                    int currentColorR = (int)Math.round(start.getRed() + (end.getRed() - start.getRed()) * t);
-                    int currentColorB = (int)Math.round(start.getBlue() + (end.getBlue() - start.getBlue()) * t);
-                    int currentColorG = (int)Math.round(start.getGreen() + (end.getGreen() - start.getGreen()) * t);
-                    g.setColor(new Color(currentColorR,currentColorG,currentColorB));
-                }
-                g.drawLine(i, j, i, j);
-            }
-        }
+        // for (int j = 0; j <= 50; j++){
+        //     for (int i = 0; i <= 550; i++){
+        //         if (i > 0){
+        //             float t = (float)i / 550;
+        //             // TODO: refactor the following into a method
+        //             int currentColorR = (int)Math.round(start.getRed() + (end.getRed() - start.getRed()) * t);
+        //             int currentColorB = (int)Math.round(start.getBlue() + (end.getBlue() - start.getBlue()) * t);
+        //             int currentColorG = (int)Math.round(start.getGreen() + (end.getGreen() - start.getGreen()) * t);
+        //             g.setColor(new Color(currentColorR,currentColorG,currentColorB));
+        //         }
+        //         g.drawLine(i, j, i, j);
+        //     }
+        // }
         return bufferedImage;
      }
 
